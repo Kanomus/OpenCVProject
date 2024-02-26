9a493a875a84454bc10acd88e4bb7015eb611d7c
Kanomus <kshagarwal373@gmail.com>
2024-02-27 00:27:37 +0530

    video resize

--- medialaxis.py
+++ medialaxis.py
@@ -5,8 +5,9 @@ vid = cv2.VideoCapture("Videos/1.mp4")
 
 while(1):
     _, img = vid.read()
-    cv2.imshow("Video",img)
-    if cv2.waitKey(30) &0xff == 27:
+    feed=cv2.resize(img,(0,0),fx=0.5,fy=0.5)
+    cv2.imshow("Video",feed)
+    if cv2.waitKey(30) == 27:
         break
 
 cv2.destroyAllWindows()
